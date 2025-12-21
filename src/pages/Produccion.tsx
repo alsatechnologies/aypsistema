19e403b6baefe859f196a1bed76e26bd81d435bd
Jorge Aldapa <jorge@example.com>
2025-12-21 15:46:48 -0700

    fix: Cambiar unidad de Kg a % en columnas de tanques (Nivel y Gomas)

--- src/pages/Produccion.tsx
+++ src/pages/Produccion.tsx
@@ -352,8 +352,8 @@ const Produccion = () => {
                       <TableHeader>
                         <TableRow>
                           <TableHead>Tanque</TableHead>
-                          <TableHead className="text-right">Nivel (Kg)</TableHead>
-                          <TableHead className="text-right">Gomas (Kg)</TableHead>
+                          <TableHead className="text-right">Nivel (%)</TableHead>
+                          <TableHead className="text-right">Gomas (%)</TableHead>
                         </TableRow>
                       </TableHeader>
                       <TableBody>
